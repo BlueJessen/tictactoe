f7d13e8017ef1412d455f8f0423459890658565b
Blue Jessen <canealisblue@gmail.com>
2022-04-19 21:46:05 -0700

    Add html skeleton

--- game.js
+++ game.js
@@ -1,23 +1,11 @@
 const player = require('./player.js');
 class Game {
   constructor() {
-
   }
-  initializeBoard(board) {
-    board = [[0,0,0],[0,0,0],[0,0,0]];
+  initializeGame(gameBoard) {
+    gameBoard = [[0,0,0],[0,0,0],[0,0,0]];
   }
-  checkWinCondition(board) {
-  var player1Check = '';
-  var player2Check = '';
-  for (var i = 0; i < board.length; i++) {
-      this.checkWinCondition(board[i]);
-      if(board[i] === player1.icon) {
-        player1Check += board[i];
-      }else if (board[i] === player2.icon) {
-        player2Check += board[i];
-      }
-  }
-  }
+  checkForWin() {
 }
 
 module.exports = Game;
