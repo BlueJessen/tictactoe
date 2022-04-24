177efa332c20caff89be46c08b0338aa4bef9745
Blue Jessen <canealisblue@gmail.com>
2022-04-24 08:52:53 -0700

    Refactor and fix win condition

--- game.js
+++ game.js
@@ -1,68 +1,78 @@
-
 class Game {
   constructor() {
     this.players = [];
     this.playerTurn = 0;
     this.gameEnd = false;
     this.gameResult = '';
-    this.gameBoard = [0,0,0,0,0,0,0,0,0];
-    this.winConditions = {combo1:['A','B','C'],
-                          combo2:['D','E','F'],
-                          combo3:['G','H','I'],
-                          combo4:['A','D','G'],
-                          combo5:['B','E','H'],
-                          combo6:['C','F','I'],
-                          combo7:['A','E','I'],
-                          combo8:['G','E','C'];
+    this.board = [null, null, null, null, null, null, null, null, null];
+    this.winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ];
   }
 
   turnSwitch() {
-    if (this.playerTurn === 0){
+    if (this.playerTurn === 0) {
       this.playerTurn = 1;
-    }else if(this.playerTurn === 1){
+    } else if (this.playerTurn === 1) {
       this.playerTurn = 0;
     }
   }
 
-  addPlayer(id, icon) {
-    var player = new Player(id,icon);
+  addPlayer(icon, innerToken) {
+    var player = new Player(icon, innerToken);
     this.players.push(player);
   }
 
-  takeTurn(index, quad) {
-          this.players[index].choice += quad;
-          this.checkWinCondition(index);
-          this.turnSwitch();
+  takeTurn(playerChoice) {
+    this.board[playerChoice] = this.players[this.playerTurn].innerToken;
+    this.checkWinCondition();
+    this.turnSwitch();
   }
 
-  checkWinCondition(index) {
-        if(this.players[index].choice.includes(this.combo1.value){
-        this.winAction(winCheck, index);
-          }
-          this.checkForCatsGame();
+  checkWinCondition() {
+    for (var i = 0; i < this.winConditions.length; i++) {
+      var indices = this.winConditions[i];
+      var checkSet = new Set([this.board[indices[0]], this.board[indices[1]], this.board[indices[2]]]);
+      if (checkSet.size === 1 && (checkSet.has("X") || checkSet.has("O"))) {
+          this.winAction(this.board[indices[0]]);
+      }
+    }
+      this.checkForCatsGame();
   }
 
-   winAction(score, index) {
-     if (score === 3 ) {
-     this.gameEnd = true;
-         this.players[index].increaseWins();
-         this.gameResult = `Player ${this.players[index].id} Wins!`;
-       }
-       this.checkForCatsGame();
-     }
+  winAction(innerToken) {
+    console.log("51", this.gameEnd);
+    this.gameEnd = true;
+    if (innerToken === "X") {
+      this.players[0].increaseWins();
+      this.gameResult = `Player 1 Wins!`;
+    }
+    if (innerToken === "O") {
+      this.players[1].increaseWins();
+      this.gameResult = `Player 2 Wins!`;
+    }
+  }
+
 
   resetGame() {
     for (var i = 0; i < this.players.length; i++) {
       this.players[i].choice = '';
       this.gameEnd = false;
     }
+    this.board = [null, null, null, null, null, null, null, null, null];
   }
 
   checkForCatsGame() {
-    if (this.players[0].choice.length === 5 || this.players[1].choice.length === 5) {
+    if (!this.board.includes(null)) {
       this.gameEnd = true;
       this.gameResult = `It was a draw! Too Bad...`;
     }
   }
-
 }
